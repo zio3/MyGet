dc08fd56edab04ada1147e345d46e47207a6007c
akihiro taguchi <info@zio3.net>
2014-05-24 14:25:24 +0900

    再度自動デプロイ確認

--- MvcHelper/MvcHelper/Dynamic.cs
+++ MvcHelper/MvcHelper/Dynamic.cs
@@ -25,7 +25,6 @@ namespace System.Linq.Dynamic
                     new Type[] { source.ElementType },
                     source.Expression, Expression.Quote(lambda)));
         }
-
         public static IQueryable Select(this IQueryable source, string selector, params object[] values) {
             if (source == null) throw new ArgumentNullException("source");
             if (selector == null) throw new ArgumentNullException("selector");
@@ -36,7 +35,6 @@ namespace System.Linq.Dynamic
                     new Type[] { source.ElementType, lambda.Body.Type },
                     source.Expression, Expression.Quote(lambda)));
         }
-
         public static IQueryable<T> OrderBy<T>(this IQueryable<T> source, string ordering, params object[] values) {
             return (IQueryable<T>)OrderBy((IQueryable)source, ordering, values);
         }
